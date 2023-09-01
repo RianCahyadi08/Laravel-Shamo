52bbf3ba2ee35ea9d1e2a93d37c6f81bb71ae296
RianCahyadi08 <ryancahyadi11@gmail.com>
2023-09-02 00:52:13 +0700

    Add route middleware fetch Create function fetch

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -88,4 +88,9 @@ class UserController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(), 'Data profile user successfully fetch');
+    }
 }
